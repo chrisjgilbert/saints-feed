058ed7e30b4cc29e364612d027a5d75a545ad6a7
Chris Gilbert <chris@gosupercritical.com>
2024-04-18 14:55:41 +0100

    Tweaks

--- app/models/source.rb
+++ app/models/source.rb
@@ -2,4 +2,9 @@ class Source < ApplicationRecord
   has_many :articles, dependent: :destroy
 
   validates :name, presence: true
+
+  # TODO: brittle
+  def logo_path
+    "sources/#{name.parameterize(separator: "_")}.png"
+  end
 end
